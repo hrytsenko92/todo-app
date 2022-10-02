9e6b79e5535a34cbbbca101bb8a08bc29b62cfa4
hrytsenko92 <m.hrytsenko1820@gmail.com>
2022-10-02 20:21:58 +0300

    view component: add usestate, add todo render list

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,9 +1,8 @@
-import React, {useEffect, useState} from 'react'
-import {useSelector} from 'react-redux'
-
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 function View() {
-  const todos = useSelector((state)=> state.todo.todos);
+  const todos = useSelector((state) => state.todo.todos);
   const [data, setData] = useState([]);
   const [isSortedStat, setIsSortedStat] = useState(false);
   const [isSortedDate, setIsSortedDate] = useState(false);
@@ -12,17 +11,19 @@ function View() {
   const sortStatTodo = () => {};
   const sortDateTodo = () => {};
 
-console.log(todos)
-
-  useEffect(()=> {
-const transformDate = () => {
-
+  useEffect(() => {
+    setData(todos)
+  }, [todos]);
+  console.log(data)
+  return (<div>
+    <span>View component</span>
+    <ul>
+      {data[0] === undefined ? <div>List is empty</div> : data.map(item => {return (<li key={item.id}><span>is:{item.isDone}</span><span>{item.title}</span><span>Priority: {item.priority}</span><span>{new Date(item.date).toLocaleString()}</span></li>)})}
+    </ul>
+    </div>);
 }
-  }, [])
 
-  return (
-  <div>view</div>
-  )
-}
+export default View;
 
-export default View
+
+// сортування по даті і пріоритеті. btn виконано і видалити. класи і стилі для чекбоксів
